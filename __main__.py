9fd4dbacc73cba32222cde6c4746effff060947a
Asheesh <ashrj@outlook.com>
2014-06-30 13:29:03 +0530

    Daemon fixed hence no need for rc.local

--- __main__.py
+++ __main__.py
@@ -3,22 +3,22 @@
 import time
 import json
 
-from src.api_interface import *
-from src.gui_interface import *
+#from src.api_interface import *
+import src.gui_interface
 
-def notify_start():
+#def notify_start():
     
-    line1 = "Touchscreen handler is activated."
-    time_delay = 5000  #in miliseconds
+#    line1 = "Touchscreen handler is activated."
+#    time_delay = 5000  #in miliseconds
     
 #    ('Notification(%s, %s, %d, %s)'%(__addonname__,line1, time_delay, __icon__))
 
 
-#Add-on Execution Starts
 #notify_start()   
-start_gui()
 
-while (True): update_gui()
+src.gui_interface.start_gui()
+
+while True: src.gui_interface.update_gui()
 
 #pygame.event.Event(QUIT)
 
